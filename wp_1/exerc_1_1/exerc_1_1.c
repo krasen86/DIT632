ef9af4a9c55665fa1f2e14d36ad87d7a3d3b62f1
eemilj <jeskanen.eemil@gmail.com>
2021-01-20 13:27:32 +0200

    Error handling, fix loop to not break on character entered, fix loop to break on only "\n",

--- wp_1/exerc_1_1/exerc_1_1.c
+++ wp_1/exerc_1_1/exerc_1_1.c
@@ -6,41 +6,47 @@
 #define SENTENCE_FOUR  "You selected 4 \n"
 #define SENTENCE_FIVE  "You selected 5 \n"
 #define SENTENCE_WRONG_INPUT "Not a valid input \n"
-
+#define SIZE_OF_LINE 256
 
 int main() {
-
+    char input[SIZE_OF_LINE];
     int selectedOption;
     do
     {
-       printf("Please enter a number between 1 and 5.\n");
-       selectedOption = getchar();
-       if (selectedOption == EOF ) 
-       {
-           printf(SENTENCE_WRONG_INPUT);
-           return 0;
-       } 
-       switch (selectedOption)
-       {
-       case '1':
-           printf(SENTENCE_ONE);
-           break;
-        case '2':
-           printf(SENTENCE_TWO);
-           break;
-        case '3':
-           printf(SENTENCE_THREE);
-           break;
-        case '4':
-           printf(SENTENCE_FOUR);
-           break;
-        case '5':
-           printf(SENTENCE_FIVE);
-           break;
-       default:
+        printf("Please enter a number between 1 and 5.\n");
+        fgets(input, sizeof (input), stdin);
+
+        if(sscanf(input, "%d", &selectedOption) != 1){
             printf(SENTENCE_WRONG_INPUT);
-           break;
-       }  
-    } while (selectedOption > '0' && selectedOption < '6');
+            return 0;
+        }
+
+        if (selectedOption == EOF )
+        {
+            printf(SENTENCE_WRONG_INPUT);
+            return 0;
+        }
+        switch (selectedOption)
+        {
+            case 1:
+                printf(SENTENCE_ONE);
+                break;
+            case 2:
+                printf(SENTENCE_TWO);
+                break;
+            case 3:
+                printf(SENTENCE_THREE);
+                break;
+            case 4:
+                printf(SENTENCE_FOUR);
+                break;
+            case 5:
+                printf(SENTENCE_FIVE);
+                break;
+            default:
+                printf(SENTENCE_WRONG_INPUT);
+                break;
+        }
+    } while (selectedOption > 0 && selectedOption < 6);
     return 0;
 }
