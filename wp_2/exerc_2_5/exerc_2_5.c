d17497b1c031d9039f6b29ff5086a2e1b5fb201f
Krasen Parvanov <krasenparvanov@gmail.com>
2021-01-30 16:44:37 +0100

    Add skeleton for 2_5

---
+++ wp_2/exerc_2_5/exerc_2_5.c
@@ -0,0 +1,36 @@
+/* ====================================
+File name: exerc_2_5.c (or cpp)
+Date: 2021-xx-xx TODO update date
+Group nr 14
+Members that contribute to the solutions: Krasen Parvanov, Chrysostomos Tsagkidis, Eemil Jeskanen
+Member not present at demonstration time:
+Demonstration code: xxxx TODO replace with code from TA
+====================================== */
+
+// Include section
+#include "stdio.h"
+#include <stdlib.h>
+#include <time.h>
+// Define section
+#define MAX 100
+#define MAX_NUMBER 20
+
+// ------ Function declaration ----------
+void create_random( int *tab );
+// Use pointer to fill the table
+
+void count_frequency(int *tab, int *freq ); // Use pointer
+void draw_histogram(int *freq );
+
+/* ==================================== Main program section ====================================== */
+/* This program ..... */
+
+int main ( void){
+    // Variable declarations
+    int table[MAX], n ;
+    int frequency[MAX_NUMBER];
+
+    return 0; // Exit the program
+}
+
+
