c80fa89e47c0be2a3873a71346f207806cefbe39
Krasen Parvanov <krasenparvanov@gmail.com>
2021-02-02 23:29:25 +0100

    Add comments to exerc_2_7.c

--- wp_2/exerc_2_7/exerc_2_7.c
+++ wp_2/exerc_2_7/exerc_2_7.c
@@ -14,8 +14,8 @@ Demonstration code: xxxx TODO replace with code from TA
 #include "ctype.h"
 
 // Define section
-#define SIZE_PERSONAL_NUMBER 10
-#define SIZE_USER_INPUT 64
+#define SIZE_PERSONAL_NUMBER 10 // define size of personal number in format yymmddxxxx
+#define SIZE_USER_INPUT 64 // accepted size of user input
 #define CORRECT_NUMBER_MESSAGE "You entered a valid personal number.\n"
 #define WRONG_NUMBER_MESSAGE "You entered an invalid personal number.\n"
 #define WRONG_PARAMETERS_MESSAGE "The number you entered was not in format <yymmddxxxx>\n"
@@ -34,34 +34,35 @@ Demonstration code: xxxx TODO replace with code from TA
 #define DECEMBER 12
 
 /* ==================================== Main program section ====================================== */
-/* This program ..... */
+/* This program TODO ..... */
 
 // Function declaration
 
-int readPersonalNumber( char *person);
-int controlDigit( const char * persnr);
-int controlDate(char *person);
-int inputValidation(char *input);
-int checkLeapYear(int year);
-int castToInt(char c);
+int readPersonalNumber( char *person); // Used to read user input
+int controlDigit( const char * persnr); // check if the last digit of a given number is valid
+int controlDate(char *person); // used to check if the date is valid
+int inputValidationInteger(char *input); // checks if the input was integer numbers
+int checkLeapYear(int year); // check if a given year is leap
+int castToInt(char c); // cast a char to integer
 
 
 int main() {
     // Variable declarations
-    char personalNumber[SIZE_PERSONAL_NUMBER];
-    int keepReading = FALSE;
-    keepReading = readPersonalNumber(personalNumber);
+    char personalNumber[SIZE_PERSONAL_NUMBER]; // used to store the p-number
+    int keepReading = FALSE; // boolean to control the main loop
+    keepReading = readPersonalNumber(personalNumber); // call function to read user input
+    // Main loop the loop runs while the user doesn't input "q" than the read method returns false and the loop is terminated
     while (keepReading) {
-        if(controlDate(personalNumber)) {
-            if (controlDigit(personalNumber)) {
-                printf(CORRECT_NUMBER_MESSAGE);
+        if(controlDate(personalNumber)) { // call method to verify that the number contains a valid year-month-day in the first 6 positions
+            if (controlDigit(personalNumber)) { // call method to check if the last digit is a valid control digit
+                printf(CORRECT_NUMBER_MESSAGE); // if valid confirm
             } else {
-                printf(WRONG_NUMBER_MESSAGE);
+                printf(WRONG_NUMBER_MESSAGE); // if control digit is wrong notify user
             }
-        } else {
+        } else { // if not a valid date print wrong number message to user
             printf(WRONG_NUMBER_MESSAGE);
         }
-        keepReading = readPersonalNumber(personalNumber);
+        keepReading = readPersonalNumber(personalNumber); // call function to continue reading personal number from user again
     }
     printf(EXIT_MESSAGE); // Print goodbye message
     return 0; // Exit the program
@@ -69,32 +70,35 @@ int main() {
 
 //----------Functions Section------------
 
+// function used to read user input. The function keeps asking for input until it doesn't get 10 integer numbers
 int readPersonalNumber(char *person) {
-    printf(PROMP_INPUT_MESSAGE);
-    char input[SIZE_USER_INPUT];
-    fgets(input,SIZE_USER_INPUT, stdin);
-    if(input[0] == EXIT_CHAR && strlen(input) == 2 ){
-        return FALSE;
-    }else if(strlen(input) != SIZE_PERSONAL_NUMBER + 1 || !inputValidation(input)) {
-        printf(WRONG_PARAMETERS_MESSAGE);
-        readPersonalNumber(person);
-    }else {
-        strcpy(person, input);
-        return TRUE;
+    printf(PROMP_INPUT_MESSAGE); // ask user for input
+    char input[SIZE_USER_INPUT]; // store input
+    fgets(input,SIZE_USER_INPUT, stdin); // read input from buffer and store it in the array
+    if(input[0] == EXIT_CHAR && strlen(input) == 2 ){ // check if the user has entered exit_char
+        return FALSE; // return false when exit char is received in order to terminate the main loop
+    }else if(strlen(input) != SIZE_PERSONAL_NUMBER + 1 || !inputValidationInteger(input)) { // if the user has not entered valid amount of char
+                                                                                            // call method to verify if it's integer input
+                                                                                            // if the method returns false(not integers present in array)
+        printf(WRONG_PARAMETERS_MESSAGE);                                                   // notify user for wrong input
+        readPersonalNumber(person);                                                         // use recursion to keep reading input
+    }else { // Else input is right size and all integers
+        strcpy(person, input); // copy input from the array into the main store array
+        return TRUE; // return true to main - keeps the main loop running
     }
-    return FALSE;
+    return FALSE; // if something is wrong return false to terminate the main loop
 }
-
-int inputValidation(char *input) {
-    while (*input) {
-        if(*input != '\n') {
-            if (isdigit(*input) == 0) {
-                return FALSE;
+// Function to validate if a given string consists of only integers
+int inputValidationInteger(char *input) {
+    while (*input) { // loop in the array using the pointer
+        if(*input != '\n') { // ignore the end of line for the comparison
+            if (isdigit(*input) == 0) { // check if the char is not digit isdigit function
+                return FALSE; // if not digit return false
             }
         }
-        input++;
+        input++; // increment the pointer
     }
-    return TRUE;
+    return TRUE; // if the loop run without encountering char return true
 }
 
 int controlDate(char *person) {
@@ -146,27 +150,29 @@ int checkLeapYear(int year) {
 }
 
 int controlDigit(const char *persnr) {
-    // 8112289874 6709199530
+    // 8112289874 6709199530 TODO remove
     int i;
-    int sum = 0;
-    long temp1 = 0, temp2 = 0, temp3;
+    long totalSum = 0;
+    long sumOfNumber = 0;
+    long firstDigitOfTheSumOfNumber = 0;
+    long secondDigitOfTheSumOfNumber = 0;
 
     for (i = 0; i < SIZE_PERSONAL_NUMBER - 1; i++){
         if (i % 2 == 0) {
-            temp1 = castToInt(persnr[i]) * 2;
-            if (temp1 > 9){
-                temp2 = (temp1 / 10) % 10;
-                temp3 = temp1 % 10;
-                sum += temp2 + temp3;
+            sumOfNumber = castToInt(persnr[i]) * 2;
+            if (sumOfNumber > 9){
+                firstDigitOfTheSumOfNumber = (sumOfNumber / 10) % 10;
+                secondDigitOfTheSumOfNumber = sumOfNumber % 10;
+                totalSum += firstDigitOfTheSumOfNumber + secondDigitOfTheSumOfNumber;
             } else {
-                sum += temp1;
+                totalSum += (sumOfNumber * 1);
             }
         } else {
-            sum += castToInt(persnr[i]);
+            totalSum += castToInt(persnr[i]);
         }
     }
 
-    if (((10 - (sum % 10)) % 10) == castToInt(persnr[SIZE_PERSONAL_NUMBER - 1])) {
+    if (((10 - (totalSum % 10)) % 10) == castToInt(persnr[SIZE_PERSONAL_NUMBER - 1])) {
         return TRUE;
     }
 
@@ -174,10 +180,7 @@ int controlDigit(const char *persnr) {
 }
 
 int castToInt(char c) {
-    int num = 0;
-
-    //Substract '0' from entered char to get
-    //corresponding digit
-    num = c - '0';
-    return num;
+    int num = 0; // define the int to be return
+    num = c - '0'; // subtract '0' from entered char to get the corresponding integer
+    return num; // return the integer
 }
