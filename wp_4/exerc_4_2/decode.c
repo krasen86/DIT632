d88bddcc52dd1bd813ad794af4f470328e102685
Krasen Parvanov <krasenparvanov@gmail.com>
2021-02-15 20:02:30 +0100

    Change the logic in exerc_4_4.c to use hex values
    Update the logic method to use the defined hex value given by user instead of converting it to decimal therefore simplifying the logic.

--- wp_4/exerc_4_2/decode.c
+++ wp_4/exerc_4_2/decode.c
@@ -1,5 +1,5 @@
 /* ====================================
-File name: code.c (or cpp)
+File name: decode.c (or cpp)
 Date: 2021-xx-xx TODO update date
 Group nr 14IIm
 Members that contribute to the solutions: Krasen Parvanov, Chrysostomos Tsagkidis, Eemil Jeskanen
@@ -31,7 +31,7 @@ unsigned int getBits(unsigned int dec_val, int startPos, int numOfBits);
 
 
 int main(int argc, char *argv[]){
-    unsigned int hexDec;
+    unsigned int hexValue;
     unsigned int engine_on;
     unsigned int gear_pos;
     unsigned int key_pos;
@@ -39,12 +39,12 @@ int main(int argc, char *argv[]){
     unsigned int brake2;
 
     if(validateArgs(argc, argv)){
-        hexDec = convertHexToDecimal(argv);
-        engine_on = getBits(hexDec, 7, 1);
-        gear_pos = getBits(hexDec, 4, 3);
-        key_pos = getBits(hexDec, 2, 2);
-        brake1 = getBits(hexDec, 1, 1);
-        brake2 = getBits(hexDec, 0 , 1);
+        hexValue  = (unsigned int) strtol(argv[1], NULL, 16);
+        engine_on = getBits(hexValue, 7, 1);
+        gear_pos = getBits(hexValue, 4, 3);
+        key_pos = getBits(hexValue, 2, 2);
+        brake1 = getBits(hexValue, 1, 1);
+        brake2 = getBits(hexValue, 0 , 1);
         printf(ENGINE_ON, engine_on);
         printf(GEAR_POS, gear_pos);
         printf(KEY_POS, key_pos);
@@ -67,52 +67,11 @@ int validateArgs(int argc, char *argv[]){
     return FALSE;
 }
 
-unsigned int convertHexToDecimal(char *argv[]){
-    // https://www.geeksforgeeks.org/program-for-hexadecimal-to-decimal/
-    char *hexChar = argv[1];
-    int len = strlen(argv[1]);
-
-    // Initializing base value to 1, i.e 16^0
-    int base = 1;
-    int dec_val = 0;
-
-    // Extracting characters as digits from last character
-    for (int i=len-1; i>=0; i--)
-    {
-        // if character lies in '0'-'9', converting
-        // it to integral 0-9 by subtracting 48 from
-        // ASCII value.
-        if (hexChar[i]>='0' && hexChar[i]<='9')
-        {
-            dec_val += (hexChar[i] - 48)*base;
-            // incrementing base by power
-            base = base * 16;
-        }
-            // if character lies in 'A'-'F' , converting
-            // it to integral 10 - 15 by subtracting 55
-            // from ASCII value
-        else if (hexChar[i]>='A' && hexChar[i]<='F')
-        {
-            dec_val += (hexChar[i] - 55)*base;
-            // incrementing base by power
-            base = base*16;
-        }
-        else if (hexChar[i]>='a' && hexChar[i]<='f'){
-            dec_val += (hexChar[i] - 87)*base;
-            // incrementing base by power
-            base = base*16;
-        }
-    }
-
-    return dec_val;
-}
-
-unsigned int getBits(unsigned int dec_val, int startPos, int numOfBits){
+unsigned int getBits(unsigned int value, int startPos, int numOfBits){
     unsigned int bitValue;
     unsigned mask;
-
     mask = ((1 << numOfBits) - 1) << startPos;
-    bitValue = dec_val & mask;
+    bitValue = value & mask;
     bitValue = bitValue >> startPos;
     if((startPos == 4 && bitValue > 4) || (numOfBits == 1 && bitValue > 1) || (startPos == 2 && bitValue > 2)) {
         printf(INVALID_HEX);
