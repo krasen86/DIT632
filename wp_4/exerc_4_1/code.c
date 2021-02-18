d717594387f256ec8f7e6937f56dbf018a3b49eb
chrytsa <62544931+chrytsa@users.noreply.github.com>
2021-02-18 12:05:02 +0200

    Add comments for exercises 4.1 and 4.3

--- wp_4/exerc_4_1/code.c
+++ wp_4/exerc_4_1/code.c
@@ -17,7 +17,13 @@ Demonstration code: xxxx TODO replace with code from TA
 #define TRUE 1; // define boolean true
 
 /* ==================================== Main program section ====================================== */
-/* This program TODO..... */
+/* This program gets 5 integer values as input parameters when running it, one that defines engine_on
+ * (0,1), a second for gear_pos (0-4), a third for key_pos (0-2), a fourth for brake1 (0,1) and the
+ * fifth for brake2 (0,1). After validating the input the program returns to the user the corresponding
+ * hexadecimal value of the binary representation of the input. For example, when 1 2 2 1 1 has been
+ * entered, with a binary representation of 10101011, then the hexadecimal value of AB shall be returned
+ * to the user.
+ * */
 
 // Function declaration
 int validateInputArguments(int size, char *arguments[]);
@@ -26,56 +32,67 @@ unsigned char setParameters(int size, char *arguments[]);
 int main(int argc, char *argv[]) {
     // Variable declarations
     int validArguments = FALSE; // define boolean flag for verifying provided arguments
-    unsigned char parameters;
+    unsigned char parameters; // used to store the parameters as unsigned char
 
-    if (argc == 6) {
-        validArguments = validateInputArguments(argc, argv); // call a function to check if the parameters are within a valid bounds
-        if (validArguments) {
-            parameters = setParameters(argc, argv);
-            printf("%X \n",parameters);
+    if (argc == 6) { // check if the number of arguments is a valid one, i.e. 5 plus one for the execution of the program
+        validArguments = validateInputArguments(argc, argv); // call a function to check if the parameters are within a valid bounds and assign the returned value to validArguments variable
+        if (validArguments) { // case of the arguments being within the valid corresponding bounds
+            parameters = setParameters(argc, argv); // call the setParameters function that
+            printf("%X \n",parameters); // print the parameters' hexadecimal representation
         }
     } else { // handle invalid number of provided arguments
-        printf(INVALID_NUMBER_ARGUMENTS);
-        return 0;
+        printf(INVALID_NUMBER_ARGUMENTS); // print error message to the user
+        return 0; // exit the program in case of error
     }
     return 0; // Exit the program
 }
+
+// Function to translate the decimal values of the input parameters to binary.
 unsigned char setParameters(int size, char *arguments[]) {
-    unsigned char parameters;
-    parameters = (*arguments[5] - '0');
-    parameters |= (*arguments[1] - '0') << 7;
-    parameters |= (*arguments[2] - '0') << 4;
-    parameters |= (*arguments[3] - '0') << 2;
-    parameters |= (*arguments[4] - '0') << 1;
-    return parameters;
+    unsigned char parameters; // store the parameters
+    /* the following code block converts the decimal values of the arguments
+     * to a binary representation of it, by first applying bit shifting to
+     * the value an X number of places until it gets to the correct position.
+     * Then, by applying the logical OR operation among all the "translations",
+     * we get the final representation of the input parameters in binary.
+    */
+    parameters = (*arguments[5] - '0'); // store the last parameter's value in binary
+    parameters |= (*arguments[1] - '0') << 7; // apply bit shifting to the binary value of the first argument, then apply logical OR operation to the previous value of the parameters and store its value
+    parameters |= (*arguments[2] - '0') << 4; // apply bit shifting to the binary value of the second argument, then apply logical OR operation to the previous value of the parameters and store its value
+    parameters |= (*arguments[3] - '0') << 2; // apply bit shifting to the binary value of the third argument, then apply logical OR operation to the previous value of the parameters and store its value
+    parameters |= (*arguments[4] - '0') << 1; // apply bit shifting to the binary value of the fourth argument, then apply logical OR operation to the previous value of the parameters and store its value
+
+    return parameters; // return the parameters
 }
 
+// Function to validate the input arguments
 int validateInputArguments(int size, char *arguments[]) {
     int i = 1; // define index for loop starts from 1 in order to not count function name
     int parsedArgument; // integer representation of the argument char
 
+    // loop to iterate through the argument's values
     for(i; i < size; i++) {
-        if (arguments[i][0] == '-') {
+        if (arguments[i][0] == '-') { // exclude negative values
             return FALSE;
         }
-        if (isdigit(*arguments[i])) {
-            parsedArgument = (*arguments[i] - '0');
-            if (i == 2) {
-                if (parsedArgument > 4) {
-                    return FALSE;
+        if (isdigit(*arguments[i])) { // check if the argument is digit
+            parsedArgument = (*arguments[i] - '0'); // store the argument's value to an integer variable
+            if (i == 2) { // case of 'gear_pos' argument
+                if (parsedArgument > 4) { // check if invalid input has been entered for gear_pos, i.e. greater than 4
+                    return FALSE; // return false to the function's call
                 }
-            } else if (i == 3) {
-                if (parsedArgument > 2) {
-                    return FALSE;
+            } else if (i == 3) { // case of 'key_pos' argument
+                if (parsedArgument > 2) { // check if invalid input has been entered for key_pos, i.e. greater than 2
+                    return FALSE; // return false to the function's call
                 }
-            } else {
-                if (parsedArgument > 1) {
-                    return FALSE;
+            } else { // every other argument that takes values of 0 and 1 as valid ones
+                if (parsedArgument > 1) {  // check if its value is out of bounds
+                    return FALSE; // return false to the function's call
                 }
             }
-        } else {
-            return FALSE;
+        } else { // the argument was not a digit
+            return FALSE; // return false to the function's call
         }
     }
-    return TRUE;
+    return TRUE; // the input was valid, thus return true to the function's call
 }
--- wp_4/exerc_4_3/exerc_4_3.c
+++ wp_4/exerc_4_3/exerc_4_3.c
@@ -1,36 +1,53 @@
-
+/* ====================================
+File name: decode.c (or cpp)
+Date: 2021-xx-xx TODO update date
+Group nr 14IIm
+Members that contribute to the solutions: Krasen Parvanov, Chrysostomos Tsagkidis, Eemil Jeskanen
+Member not present at demonstration time:
+Demonstration code: xxxx TODO replace with code from TA
+====================================== */
 
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DISPLAY_PRESSED_NUMBER " --------Port value base 10 = %d | Port value base 16 = %X \n"
+#define DISPLAY_PRESSED_NUMBER " --------Port value base 10 = %d | Port value base 16 = %X \n" // String that prints the decimal and hexadecimal value of the pressed key
+
+/* ==================================== Main program section ====================================== */
+/* This program simulates the behavior of reading input from keyboard without any hardware assistance.
+ * Specifically, when a key is pressed on the keyboard an 8-bit value is saved in a specific register
+ * address, from which the first 4 bits comprise the value of a pressed key (0-15), whereas the last
+ * bit (place 7 in the register) specifies whether the key was down when the register is read, by
+ * being 0, otherwise the bit 7 is set to 1.
+ * */
 
 void f_delay(int);
 unsigned char random_inport( void);
 void printPort(int portValue, unsigned int pressedNumber );
-unsigned int getBits(unsigned char port);
+unsigned int getBits(unsigned char port, int numOfBits);
 
 int main(){
-    int nr;
     unsigned char port;
     unsigned int number;
     srand(time(0));
-    while (1){
+    // main loop for keep 'reading' for simulated input
+    while (1) {
         port = random_inport();
-        if ((port >> 7) == 0b0) {
-            number = getBits(port);
-            printPort(port, number);
+        if ((port >> 7) == 0b0) { // check if the bit 7 in the register is pressed by comparing its value equal to 0
+            number = getBits(port, 4); // get the binary representation of the bits that specify the value of a pressed key, i.e. last 4 bits
+            printPort(port, number); // display the pressed key's port number and its value
         }
         f_delay(5);
     }
 }
-unsigned int getBits(unsigned char port){
-    unsigned int bitValue;
-    unsigned mask;
-    mask = ((1 << 4) - 1);
-    bitValue = port & mask;
-    return bitValue;
+
+// Function to get the specific bits needed from a provided binary value
+unsigned int getBits(unsigned char port, int numOfBits){
+    unsigned int bitValue; // store the value into an integer
+    unsigned mask; // store the bit values that we want to keep from the initial number
+    mask = ((1 << numOfBits) - 1); // mask used in order to isolate the last 4 bits from the number
+    bitValue = port & mask; // save the port and mask values into the bitValue applying bitwise AND
+    return bitValue; // return the value to the function call
 }
 
 void f_delay( int tenth_sec){
@@ -63,7 +80,7 @@ void printPort(int portValue, unsigned int pressedNumber ){
     // Print bits and at the end corresponding decimal value
     for(j =i-1 ;j > -1;j--)
         printf("  %d",binChar[j]);
-    printf(DISPLAY_PRESSED_NUMBER, pressedNumber, pressedNumber);
+    printf(DISPLAY_PRESSED_NUMBER, pressedNumber, pressedNumber); // displaying the decimal and hexadecimal value of the pressed key
 }
 
 
