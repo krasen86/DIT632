ff4c56fb24a8c0b9cbb5fd374465e7929fc90979
eemilj <jeskanen.eemil@gmail.com>
2021-03-08 16:59:52 +0200

    Work in progress, add connectivity between server and client

---
+++ client_server/client.c
@@ -0,0 +1,55 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "person.h"
+
+int main(){
+    int iSocket;
+    struct sockaddr_in server_addr;
+    char *server_message;
+    int PORT = 8080;
+    server_message = malloc(2000 * sizeof (char));
+
+    // Create socket:
+    iSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (iSocket < 0) {
+        printf("Unable to create socket\n");
+        return -1;
+    }
+
+    printf("Socket created successfully\n");
+
+    // Set port and IP the same as server-side:
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Send connection request to server:
+    if (connect(iSocket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        printf("Unable to connect\n");
+        return -1;
+    }
+    printf("Connected with server successfully\n");
+
+    // Receive the server's response:
+    if (recv(iSocket, server_message, sizeof(server_message), 0) < 0) {
+        printf("Error while receiving server's msg\n");
+        return -1;
+    }
+    printf("%d\n", *server_message);
+
+    memset(server_message, '\0', sizeof(*server_message));
+
+    if (recv(iSocket, server_message, sizeof(server_message), 0) < 0) {
+        printf("Error while receiving server's msg\n");
+        return -1;
+    }
+    Person *person = (Person*) server_message;
+    printf("%s\n", person->personFirstName);
+    close(iSocket);
+}
--- client_server/server.c
+++ client_server/server.c
@@ -1,26 +1,118 @@
 #include "person.h"
 #include "string.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
 
 
 void printList();
+void populateList();
 
 int main(){
-    Person person;
-    strcpy(person.personFirstName,"Krasen");
-    strcpy(person.personLastName,"TheKraken");
-    insertPerson(person);
+    populateList();
     printList();
+
+    int server_fd;      // server file description
+    int iSocket;        // socket id
+    struct sockaddr_in addrSocket;  // socket's address
+    int opt = 1;         // broadcast, which is a standard protocol for IPv4
+    int PORT = 8080;
+
+    int iAddrLen = sizeof(addrSocket);
+
+    int* strReply = malloc(sizeof (int));
+    *strReply = getNumberOfPersons();
+    // Creating socket file descriptor
+    // IPv4 and TCP
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        printf("Error: Could not create socket: %s. Exiting...\n", strerror(errno));
+        exit(1);
+    }
+
+    // Attaching the socket to our address
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        printf("Error: Could not attach: %s. Exiting...\n", strerror(errno));
+        exit(1);
+    }
+
+    // specifying the protocols and addresses from which we accept the connections
+    addrSocket.sin_family = AF_INET;            // protocol
+    addrSocket.sin_addr.s_addr = INADDR_ANY;    // address
+    addrSocket.sin_port = htons(PORT);          // port
+
+    // attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *) &addrSocket, iAddrLen) < 0) {
+        printf("Error: Could not bind: %s. Exiting...\n", strerror(errno));
+        exit(1);
+    }
+    // now we just wait and listen for the connections from clients
+    // the second parameter is the number of connections to be put in queue before rejecting them
+    // here, we put 1
+    if (listen(server_fd, 1) < 0) {
+        printf("Error: Could not listen: %s. Exiting...\n", strerror(errno));
+        exit(1);
+    }
+
+    // once we get a connect, we accept it
+    iSocket = accept(server_fd, (struct sockaddr *) &addrSocket, (int *) &iAddrLen);
+
+    // for fail-safe program, we check if the socket is actually created and the connection is accepted
+    if (iSocket < 0) {
+        printf("Error: Could not accept a connection: %s. Exiting...\n", strerror(errno));
+        exit(1);
+    }
+    // send something over the socket
+    if (send(iSocket, strReply, 1, 0) < 0) {
+        printf("Error sending the message to the client %s. Not really a problem on our side. \n", strerror(errno));
+    }
+
+    Person person = head ->person;
+    if (send(iSocket, &person, strlen(&person), 0) < 0) {
+        printf("Error sending the message to the client %s. Not really a problem on our side. \n", strerror(errno));
+    }
+
+
+    close(server_fd);
+
+
 }
 
 void printList(){
     List *temp = head;
+    int i = 0;
     if(temp == NULL){
         printf("List is empty");
     }else {
         while (temp != NULL) {
-            printf("%s\n", temp -> person.personFirstName);
+            i++;
+            printf("%d: %s ", i, temp -> person.personFirstName);
             printf("%s\n", temp -> person.personLastName);
             temp = temp -> next;
         }
     }
+}
+
+void populateList(){
+    Person person;
+    strcpy(person.personFirstName, "Krasen");
+    strcpy(person.personLastName, "Parvanov");
+    insertPerson(person);
+    Person person2;
+    strcpy(person2.personFirstName, "Chrysostomos");
+    strcpy(person2.personLastName, "Tsagkidis");
+    insertPerson(person2);
+    Person person3;
+    strcpy(person3.personFirstName, "Eemil");
+    strcpy(person3.personLastName, "Jeskanen");
+    insertPerson(person3);
+    Person person4;
+    strcpy(person4.personFirstName, "Donald");
+    strcpy(person4.personLastName, "Trump");
+    insertPerson(person4);
+    Person person5;
+    strcpy(person5.personFirstName, "Barack");
+    strcpy(person5.personLastName, "Obama");
+    insertPerson(person5);
+
 }
